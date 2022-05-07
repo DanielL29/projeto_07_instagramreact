8cc413bad631ea77231057b036de91b0abc3bfb5
DanielL29 <danielnatsufire@gmail.com>
2022-05-07 11:24:27 -0300

    bonus - dar like no post

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import './Comment.css';
 
 export default function Comment(props) {
+    const [color, setColor] = useState(false)
+
     return (
-        <div className="comment">
+        <div className={`comment ${color === true ? 'red' : ''}`}>
             <p>
                 <strong>{props.commentName}</strong> {props.message}
             </p>
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name={`${color === true ? 'heart' : 'heart-outline'}`} 
+                onClick={() => setColor(!color)}>
+            </ion-icon>
         </div>
     )
 }
--- src/components/posts-card/PostsCard.jsx
+++ src/components/posts-card/PostsCard.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import './PostsCard.css'
 
 export default function PostsCard(props) {
+    const [color, setColor] = useState(false)
+
     return (
         <div className="card">
             <div className="card-top">
@@ -9,20 +11,21 @@ export default function PostsCard(props) {
                 <h3>{props.userName}</h3>
                 <ion-icon name="ellipsis-horizontal"></ion-icon>
             </div>
-            <div className="card-image">
+            <div className="card-image" onClick={() => setColor(!color)}>
                 {props.isVideo ? (
                     <video autoPlay muted>
-                        <source src={`./assets/videos/${props.postImage}.mp4"`} type="video/mp4" />
-                        <source src={`./assets/videos/${props.postImage}.ogv"`} type="video/ogg" />
+                        <source src={`./assets/videos/${props.postImage}.mp4`} type="video/mp4" />
+                        <source src={`./assets/videos/${props.postImage}.ogv`} type="video/ogv" />
                   </video>
                 ) : (
                     <img src={`./assets/images/${props.postImage}`} alt="post" />
                 )}
             </div>
             <div className="card-info">
-                <div className="info-icons">
+                <div className={`info-icons ${color === true ? 'red' : ''}`}>
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name={`${color === true ? 'heart' : 'heart-outline'}`} 
+                            onClick={() => setColor(!color)}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
